d8b9d6d1ba7df116c4011e265ded3ccfd9a87afd
ducbet <ducbet1996@gmail.com>
2017-10-18 00:48:25 +0700

    sign_up_with_more_2_columns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    debugger
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -22,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :birthday, :gender)
   end
 end
